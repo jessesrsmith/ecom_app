6164c8ccd49aa7f06787957ef3e7351941243542
Jesse Smith <jessesrsmith2@gmail.com>
2015-11-10 10:02:40 -0700

    Finish static pages skeleton

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'static_pages#home'
-
   get 'static_pages/help'
-
   get 'static_pages/about'
-
   get 'static_pages/contact'
 
   # The priority is based upon order of creation: first created -> highest priority.
