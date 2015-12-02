f0cbaeab92b5fbd13d722655c599cf9c6f487825
Jesse Smith <jessesrsmith2@gmail.com>
2015-12-02 09:32:25 -0700

    Add initial signup tests

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,24 +4,24 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Ecom App"
+    assert_select "title", full_title
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Help | Ecom App"
+    assert_select "title", full_title("Help")
   end
 
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "About | Ecom App"
+    assert_select "title", full_title("About")
   end
 
   test "should get contact" do
     get :contact
     assert_response :success
-    assert_select "title", "Contact | Ecom App"
+    assert_select "title", full_title("Contact")
   end
 end
