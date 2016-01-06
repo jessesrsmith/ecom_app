e7cb10f6b0b44a9424b92a408beda41cfe62625b
Jesse Smith <jessesrsmith2@gmail.com>
2016-01-06 11:14:30 -0700

    Add persistent session tests

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -39,4 +39,14 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
   test "authenticated should return false for a user with a nil digest" do
     assert_not @user.authenticated?('')
   end
+
+  test "login with remembering" do
+    log_in_as(@user, remember_me: '1')
+    assert_equal cookies['remember_token'], assigns(:user).remember_token
+  end
+
+  test "login without remembering" do
+    log_in_as(@user, remember_me: '0')
+    assert_nil cookies['remember_token']
+  end
 end
