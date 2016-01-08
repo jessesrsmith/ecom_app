01d4b75aeb46a5dafb5bc3da2d809c9b8d6f097d
Jesse Smith <jessesrsmith2@gmail.com>
2016-01-08 11:22:09 -0700

    Add authorization to user profile editing

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -1,11 +1,13 @@
 require "test_helper"
 
 class UsersEditTest < ActionDispatch::IntegrationTest
+  
   def setup
     @user = users(:michael)
   end
 
   test "unsuccessful edit" do
+    log_in_as(@user)
     get edit_user_path(@user)
     assert_template 'users/edit'
     patch user_path(@user), user: { name:  "",
@@ -15,9 +17,10 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     assert_template 'users/edit'
   end
 
-  test "successful edit" do
+ test "successful edit with friendly forwarding" do
     get edit_user_path(@user)
-    assert_template 'users/edit'
+    log_in_as(@user)
+    assert_redirected_to edit_user_path(@user)
     name  = "Foo Bar"
     email = "foo@bar.com"
     patch user_path(@user), user: { name:  name,
