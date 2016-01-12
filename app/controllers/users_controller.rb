1dfe3c29931a4049bac04700800c402fdc42fc1a
Jesse Smith <jessesrsmith2@gmail.com>
2016-01-12 13:12:11 -0700

    Add security to users#show route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :update]
-  before_action :correct_user,   only: [:edit, :update]
+  before_action :logged_in_user, only: [:edit, :update, :show]
+  before_action :correct_user,   only: [:edit, :update, :show]
 
   def show
     @user = User.find(params[:id])
@@ -29,7 +29,7 @@ class UsersController < ApplicationController
       flash[:success] = "Profile updated"
       redirect_to @user
     else
-      render 'edit'
+      render "edit"
     end
   end
 
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -17,12 +17,6 @@ class UsersControllerTest < ActionController::TestCase
     assert_redirected_to login_url
   end
 
-  test "should redirect update when not logged in" do
-    patch :update, id: @user, user: { name: @user.name, email: @user.email }
-    assert_not flash.empty?
-    assert_redirected_to login_url
-  end
-
   test "should redirect edit when logged in as wrong user" do
     log_in_as(@other_user)
     get :edit, id: @user
@@ -30,10 +24,28 @@ class UsersControllerTest < ActionController::TestCase
     assert_redirected_to root_url
   end
 
+  test "should redirect update when not logged in" do
+    patch :update, id: @user, user: { name: @user.name, email: @user.email }
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
+
   test "should redirect update when logged in as wrong user" do
     log_in_as(@other_user)
     patch :update, id: @user, user: { name: @user.name, email: @user.email }
     assert flash.empty?
+    assert_redirected_to root_url
+  end
+
+  test "should redirect show when not logged in" do
+    get :show, id: @user
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
+
+  test "should redirect show when logged in as wrong user" do
+    log_in_as(@other_user)
+    get :show, id: @user
     assert_redirected_to root_url
   end
 end
