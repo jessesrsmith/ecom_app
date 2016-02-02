250d57c64baaef4c8442bf4f43e10ae25ce92ddb
Jesse Smith <jessesrsmith2@gmail.com>
2016-02-02 10:20:24 -0700

    Fix style guide violations

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,6 @@ Product.delete_all
 
 (1..12).each do
   Product.create!(title: Faker::Commerce.product_name,
-                  description: Faker::Lorem.paragraph(10),
+                  description: Faker::Lorem.paragraph(40),
                   price: Faker::Commerce.price)
 end
