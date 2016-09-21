aa1332e22c19a5e87c3277a86b5687ccf2c1a76c
Jesse Smith <jessesrsmith2@gmail.com>
2016-09-20 21:55:41 -0600

    Fix style guide violations

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -12,7 +12,7 @@ class CartsController < ApplicationController
   # GET /carts/1.json
   def show
     if @cart.line_items.empty?
-      flash.now[:warning] = 'Your cart is empty'
+      flash.now[:warning] = "Your cart is empty"
     end
   end
 
@@ -32,7 +32,7 @@ class CartsController < ApplicationController
 
     respond_to do |format|
       if @cart.save
-        format.html { redirect_to @cart, notice: 'Cart was successfully created.' }
+        format.html { redirect_to @cart, notice: "Cart was successfully created." }
         format.json { render :show, status: :created, location: @cart }
       else
         format.html { render :new }
@@ -46,7 +46,7 @@ class CartsController < ApplicationController
   def update
     respond_to do |format|
       if @cart.update(cart_params)
-        format.html { redirect_to @cart, notice: 'Cart was successfully updated.' }
+        format.html { redirect_to @cart, notice: "Cart was successfully updated." }
         format.json { render :show, status: :ok, location: @cart }
       else
         format.html { render :edit }
@@ -61,12 +61,13 @@ class CartsController < ApplicationController
     @cart.destroy if @cart.id == session[:cart_id]
     session[:cart_id] = nil
     respond_to do |format|
-      format.html { redirect_to products_url, warning: 'Your cart is empty' }
+      format.html { redirect_to products_url, warning: "Your cart is empty" }
       format.json { head :no_content }
     end
   end
 
   private
+
     # Use callbacks to share common setup or constraints between actions.
     def set_cart
       @cart = Cart.find(params[:id])
